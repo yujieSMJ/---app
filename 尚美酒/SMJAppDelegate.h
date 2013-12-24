deafdbe382c3d8a86588ecca4f1fb2e0eb05ef5d
Mac3 <macuser@tech-yujie-mac.lan>
2013-12-24 15:15:15 +0800

    Initial Commit

---
+++ 尚美酒/SMJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMJAppDelegate.h
+//  尚美酒
+//
+//  Created by Mac3 on 13-12-24.
+//  Copyright (c) 2013年 zwtym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
